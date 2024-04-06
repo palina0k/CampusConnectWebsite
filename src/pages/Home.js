edc20e9d8585ee462b7c431140ed17051923000f
palina0k <korzik.polina@gmail.com>
2024-04-06 08:14:30 -0700

    Started Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import { logRoles } from '@testing-library/react';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import './Home.css'; 
+
+export default function Home(){
+    
+}
