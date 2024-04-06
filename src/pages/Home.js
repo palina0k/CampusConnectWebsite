858ef08906fa108f2f69faf7ecd4c5f35f5ee1f7
palina0k <korzik.polina@gmail.com>
2024-04-06 10:55:08 -0700

    home page initial stages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css'; 
 
 export default function Home(){
     return(
-        <div>
-            <h1> This is a Home page</h1>
+        <div className="container">
+            <img src = "https://marcomm.sfsu.edu/sites/default/files/images/donot-marks-.jpg" alt = "San Francisco State University Logo"/>
+            <div className = "titlecontainer">
+                <h1 className="title"> Campus Connect</h1>
+                <h2 className="subtitle">Statement</h2>
+                <p className="description">Text here</p>
+                <h2 className="subtitle">Goals</h2>
+                <p className="description">text here</p>
+            </div>
+            <Link to="/post">
+                <button>Get Started</button>
+            </Link>
         </div>
     );
 }
